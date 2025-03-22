1325efb1778768be10609f91d3234621426fd150
Junya <160022550+Juna1013@users.noreply.github.com>
2025-03-22 23:11:30 +0900

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './src/app/**/*.{js,ts,jsx,tsx,mdx}', // Next.jsのApp Routerを使用する場合
-      './src/components/**/*.{js,ts,jsx,tsx,mdx}', // コンポーネントディレクトリ
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
